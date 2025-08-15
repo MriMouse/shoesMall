c757a5c2084f020f2487dbc17e4e5e0b2ae07deb
MriMouse <zuoxuefei12138@gmail.com>
2025-08-15 10:30:38 +0800

    调整axios基础URL设置为注释，并修复vue.config.js中的pathRewrite配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+//import axios from 'axios'
 
 // 设置axios基础URL（根据您的后端服务地址调整）
-axios.defaults.baseURL = 'http://localhost:8080' // 请根据您的后端服务地址修改
+//axios.defaults.baseURL = 'http://localhost:8080' // 请根据您的后端服务地址修改
 
 const app = createApp(App);
 
