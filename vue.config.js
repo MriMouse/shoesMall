ed9d4f7d3346ebc9c39eed7d6126caa7615a5e3a
lenovo <1933702055@qq.com>
2025-08-15 11:17:00 +0800

    订单

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8082, // 修改为8082，和你的前端一致
+    port: 8080, 
     proxy: {
       "/api": {
         target: "http://localhost:8081", // 后端端口为8081
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "/api",
+          "^/api": "",
         },
         logLevel: "debug", // 添加日志以便调试
       },
