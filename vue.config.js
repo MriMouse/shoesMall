b8668074ca8254e02b18be82925aacb70cbd73da
MriMouse <zuoxuefei12138@gmail.com>
2025-08-15 10:47:56 +0800

    修复axios代理问题

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8082, // 修改为8082，和你的前端一致
+    port: 8082, // 前端端口
     proxy: {
       "/api": {
-        target: "http://localhost:8081", // 后端端口为8081
+        target: "http://localhost:8081",
         changeOrigin: true,
-        pathRewrite: {
-          "^/api": "/api",
-        },
-        logLevel: "debug", // 添加日志以便调试
+        pathRewrite: { "^/api": "" },
       },
     },
   },
